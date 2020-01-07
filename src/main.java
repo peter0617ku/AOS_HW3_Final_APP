66ae15a5cf76efbd7bd44f4f5b372492d7b03d47
Peter MP Ku <43951316+peter0617ku@users.noreply.github.com>
2020-01-07 12:39:57 +0800

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,10 @@
+package ctrlc;
+
+public class main {
+	public static void main(String[] arg)
+	{
+		ctrlC a = new ctrlC();
+		a.setBookContents("FUCK!!");
+		System.out.println(a.getBookContents());
+	}
+}
